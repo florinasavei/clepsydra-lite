5cdadf7c266d5947955f932ae0e5f01752ed8c9a
Asavei Florin <asaveiflorin@gmail.com>
2020-03-06 22:31:34 +0200

    folder restructuring

--- APP/ClepsydraLite-Angular/src/app/app.component.spec.ts
+++ APP/ClepsydraLite-Angular/src/app/app.component.spec.ts
@@ -16,20 +16,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ClepsydraLiteFrontEnd'`, () => {
+  it(`should have as title 'ClepsydraLite-Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ClepsydraLiteFrontEnd');
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ClepsydraLite-Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ClepsydraLiteFrontEnd app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('ClepsydraLite-Angular app is running!');
   });
 });
