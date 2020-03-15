e3e6abc18887725009da1246f3d2821cf4f711e5
Asavei Florin <asaveiflorin@gmail.com>
2020-03-15 21:16:16 +0200

    created components

--- APP/ClepsydraLite-Angular/src/app/app.module.ts
+++ APP/ClepsydraLite-Angular/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SuppliersContainerComponent } from './components/entities/supplier/suppliers-container/suppliers-container.component';
+import { SuppliersListComponent } from './components/entities/supplier/suppliers-list/suppliers-list.component';
+import { SupplierFormComponent } from './components/entities/supplier/supplier-form/supplier-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuppliersContainerComponent,
+    SuppliersListComponent,
+    SupplierFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ APP/ClepsydraLite-Angular/src/app/components/entities/supplier/suppliers-container/suppliers-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-suppliers-container',
+  templateUrl: './suppliers-container.component.html',
+  styleUrls: ['./suppliers-container.component.scss']
+})
+export class SuppliersContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
