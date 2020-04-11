d7aec0516a2406da0ba6cded0de6116c72281186
Asavei Florin <asaveiflorin@gmail.com>
2020-04-11 12:20:35 +0300

    tried to update angular

--- APP/ClepsydraLite-Angular/src/app/app.module.ts
+++ APP/ClepsydraLite-Angular/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule }   from '@angular/forms';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomMaterialModule } from './material.module';
+
 import { SuppliersContainerComponent } from './components/entities/supplier/suppliers-container/suppliers-container.component';
 import { SuppliersListComponent } from './components/entities/supplier/suppliers-list/suppliers-list.component';
 import { SupplierFormComponent } from './components/entities/supplier/supplier-form/supplier-form.component';
@@ -19,12 +22,17 @@ import { SupplierFormComponent } from './components/entities/supplier/supplier-f
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
+    MatNativeDateModule,
+    FlexLayoutModule,
+    CustomMaterialModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
