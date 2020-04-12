c2805b358a3ef6cb0c485585837850f277094290
Asavei Florin <asaveiflorin@gmail.com>
2020-04-12 19:35:39 +0300

    added basic Angular model, service and component for Shop

--- APP/ClepsydraLite-Angular/src/app/app.module.ts
+++ APP/ClepsydraLite-Angular/src/app/app.module.ts
@@ -15,13 +15,15 @@ import { SupplierFormComponent } from './components/entities/supplier/supplier-f
 import { FontAwesomeModule,FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+import { ShopComponent } from './components/entities/shop/shop.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SuppliersContainerComponent,
     SuppliersListComponent,
-    SupplierFormComponent
+    SupplierFormComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
