d2e4a0b80a0a2787147c1a21ff7e2c21b94862cc
Florin Asavei <asaveiflorin@gmail.com>
2020-09-04 21:35:37 +0300

    testing

--- APP/ClepsydraLite-API/ClepsydraLite.API/Startup.cs
+++ APP/ClepsydraLite-API/ClepsydraLite.API/Startup.cs
@@ -1,25 +1,18 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using ClepsydraLite.API.Options;
 using ClepsydraLite.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using ClepsydraLite.DAL.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace ClepsydraLite.API
